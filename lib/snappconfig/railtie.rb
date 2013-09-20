64d181c99fc54a86bba22296f2ff90beb9e20e05
Yarin Kessler <ykessler@gmail.com>
2013-09-20 09:03:34 -0400

    Fixed premature environment cleanup in heroku task

--- lib/snappconfig/railtie.rb
+++ lib/snappconfig/railtie.rb
@@ -13,10 +13,6 @@ module Snappconfig
       appconfig.deep_merge! appconfig.fetch('defaults', {})
       appconfig.deep_merge! appconfig.fetch(Rails.env, {})
 
-      Snappconfig.environments.each do |environment|
-        appconfig.delete(environment)
-      end
-
       check_required(appconfig)
 
       # Assign ENV values...
@@ -26,6 +22,9 @@ module Snappconfig
         end
         # ... and clear ENV values from CONFIG so we don't have duplicate data:
         appconfig.delete('ENV')
+        appconfig.each do |key,value|
+          value.delete('ENV') if value.is_a?(Hash) && value.has_key?('ENV')
+        end
       end
 
       appconfig = recursively_symbolize_keys(appconfig)
@@ -40,7 +39,13 @@ module Snappconfig
     private
     
     def check_required(hash)
-      hash.each_pair do |key,value|
+      
+      env_hash = hash.clone
+      Snappconfig.environments.each do |environment|
+        env_hash.delete(environment)
+      end
+      
+      env_hash.each_pair do |key,value|
         if value == '_REQUIRED'
           raise "The configuration value #{key} is required but was not supplied. Check your application.yml file(s)."
         elsif value.is_a?(Hash)
